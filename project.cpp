b5b39e50c5f4fe33e315bf4c391ce0fd91e2df96
VG 7 <vishalgoswami49363@gmail.com>
2024-06-25 13:26:08 +0530

    Add files via upload
    a simple program to find area of a circle.

---
+++ project.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r;
+    cout << "Enter the radius of a circle : ";
+    cin >> r;
+    cout << "The area of a circle is : ";
+    cout << 3.14 * r * r << endl;
+}
